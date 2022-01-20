7d0fc75ababddbcdf96f5a1b0f5ddc12a588a672
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-01-20 14:09:05 +0300

    commit

--- FilmApp/Controllers/ViewController/ViewController.swift
+++ FilmApp/Controllers/ViewController/ViewController.swift
@@ -12,6 +12,7 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        dwdwwd
     }
 
     // MARK: - Actions
